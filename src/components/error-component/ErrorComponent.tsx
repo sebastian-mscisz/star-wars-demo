60670291f50306bb42ba8ec1b0eab8849e1277c7
sebastian-mscisz <69245075+sebastian-mscisz@users.noreply.github.com>
2023-10-29 15:51:06 +0100

    Fix spinner and error component positioning

--- src/components/error-component/ErrorComponent.tsx
+++ src/components/error-component/ErrorComponent.tsx
@@ -10,13 +10,15 @@ type ErrorComponentProps = {
 
 export const ErrorComponent: React.FC<ErrorComponentProps> = ({ code, message }) => {
     return (
-        <div className="sw-error">
-            <h1>
-                {code} {message}
-            </h1>
-            <button className="sw-error__button">
-                <NavLink to={landingPagePath()}>Go home</NavLink>
-            </button>
+        <div className="sw-error-wrapper">
+            <div className="sw-error-wrapper__content">
+                <h1>
+                    {code} {message}
+                </h1>
+                <button className="sw-error-wrapper__content__button">
+                    <NavLink to={landingPagePath()}>Go home</NavLink>
+                </button>
+            </div>
         </div>
     );
 };
